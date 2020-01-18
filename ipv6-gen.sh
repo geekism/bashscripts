adec6e5ad6630adc3edf377a9f4ab788b030e691
black <geekism@gmail.com>
2020-01-17 21:11:02 -0500

    enabled adding to ifconfig, i forgot this :/

--- ipv6-gen.sh
+++ ipv6-gen.sh
@@ -1,5 +1,5 @@
 array=( 1 2 3 4 5 6 7 8 9 0 a b c d e f )
-MAXCOUNT=20
+MAXCOUNT=5
 count=1
 network=2001:470:c1e8:
 
@@ -9,14 +9,15 @@ rnd_ip_block ()
     b=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
     c=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
     d=${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}${array[$RANDOM%16]}
-    echo $network:$a:$b:$c:$d
+    echo "added $network:$a:$b:$c:$d to he-ipv6"
+    ip -6 addr add "$network:$a:$b:$c:$d/128" dev he-ipv6
 }
 
 echo "$MAXCOUNT IPv6:"
 echo "-----------------"
-while [ "$count" -le $MAXCOUNT ]        # Генерация 20 ($MAXCOUNT) случайных чисел.
+while [ "$count" -le $MAXCOUNT ]
 do
         rnd_ip_block
-        let "count += 1"                # Нарастить счетчик.
+        let "count += 1"
         done
 echo "-----------------"
